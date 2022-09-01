e074d277e20c98d480a682f3a97999ca5a1a000d
Saaaaano <tb.25.suga@gmail.com>
2022-09-01 21:54:35 +0900

    add comment, add x undisp

--- main.py
+++ main.py
@@ -10,14 +10,17 @@ url = st.text_input('伝助のURLを入力してください。')
 if url:
     soup = fc.get_data_soup(url)
     names, header_items = fc.get_data_header(soup)
-    # disp_mark = st.sidebar.multiselect(
-    #     '表示するマーク', header_items, default=header_items)
     st.sidebar.write(fc.get_expl(soup))
+    x_undisp = st.sidebar.checkbox('× の行を表示しない')
     disp_name = st.sidebar.multiselect('表示する名前', names, default=names)
-    st.sidebar.markdown('コメント')
-    comment = fc.get_comment(soup)
-    for i in comment:
-        st.sidebar.write(i)
+    st.sidebar.markdown('**コメント**')
+    comments = fc.get_comment(soup)
+    for i in range(0, len(comments), 2):
+        with st.sidebar.expander(comments[i]):
+            comments[i+1]
     df_sc = fc.get_data_schedule(soup, names, header_items)
     data = df_sc[disp_name]
+    if x_undisp:
+        data = data[disp_name and df_sc[disp_name] != '×'].dropna(how='any')
     st.dataframe(data, height=1122)
+    st.sidebar.write('Made by Sano @20220901')
